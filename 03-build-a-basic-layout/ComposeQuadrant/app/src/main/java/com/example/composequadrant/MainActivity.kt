33526239081aa40bfd4497581a0c938317156b88
Mr-DeBonis <ignacio.debonis7@gmail.com>
2023-08-08 02:05:59 -0400

    Refactor Compose Quadrant with good practices

--- 03-build-a-basic-layout/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ 03-build-a-basic-layout/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -5,30 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,68 +52,66 @@ fun HomePagePreview() {
 
 @Composable
 fun HomePage(modifier: Modifier = Modifier) {
-    Column(
-    ) {
-        Row(
-            Modifier
-                .weight(1f)
-        ) {
-            Quadrant(
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
                 title = stringResource(id = R.string.q1_title),
                 content = stringResource(id = R.string.q1_content),
-                modifier = Modifier
-                    .weight(1f)
-                    .background(color = colorResource(id = R.color.color_1))
+                backgroundColor = colorResource(id = R.color.color_1),
+                modifier = Modifier.weight(1f)
             )
-            Quadrant(
+            ComposableInfoCard(
                 title = stringResource(id = R.string.q2_title),
                 content = stringResource(id = R.string.q2_content),
-                modifier = Modifier
-                    .weight(1f)
-                    .background(color = colorResource(id = R.color.color_2))
+                backgroundColor = colorResource(id = R.color.color_2),
+                modifier = Modifier.weight(1f)
             )
         }
-        Row(
-            Modifier
-                .weight(1f)
-        ) {
-            Quadrant(
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
                 title = stringResource(id = R.string.q3_title),
                 content = stringResource(id = R.string.q3_content),
-                modifier = Modifier
-                    .weight(1f)
-                    .background(color = colorResource(id = R.color.color_3))
+                backgroundColor = colorResource(id = R.color.color_3),
+                modifier = Modifier.weight(1f)
             )
-            Quadrant(
+            ComposableInfoCard(
                 title = stringResource(id = R.string.q4_title),
                 content = stringResource(id = R.string.q4_content),
-                modifier = Modifier
-                    .weight(1f)
-                    .background(color = colorResource(id = R.color.color_4))
+                backgroundColor = colorResource(id = R.color.color_4),
+                modifier = Modifier.weight(1f)
             )
         }
     }
 }
 
 @Composable
-fun Quadrant(title: String, content: String, modifier: Modifier = Modifier) {
+fun ComposableInfoCard(
+    title: String,
+    content: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = modifier
-            .padding(16.dp)
             .fillMaxSize()
+            .background(color = backgroundColor)
     ) {
         Text(
             text = title,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .padding(bottom = 16.dp)
+            modifier = Modifier.padding(16.dp)
         )
         Text(
             text = content,
             textAlign = TextAlign.Justify,
             modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 16.dp,
+                )
         )
     }
 }
